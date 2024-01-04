b35a807c4c53c734b72c8d6e797d88af146892ed
gaurish <nerdgaurish@gmail.com>
2024-01-04 21:49:29 +0530

    first commit

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
+const LoginPage = () => {
+  const router = useRouter();
+  const [submitError, setSubmitError] = useState('')
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,9 @@
+import React, { ReactNode } from 'react'
+
+const HomePageLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <main>{children}</main>
+  )
+}
+
+export default HomePageLayout
