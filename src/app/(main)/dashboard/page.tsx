e2d7b057cfaaf783e5b95a19d551884a04300bd4
gaurish <nerdgaurish@gmail.com>
2024-01-12 21:59:14 +0530

    added dashboardsetup component with emoji picker

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { cookies } from "next/headers";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import db from "@/lib/supabase/db";
+import { redirect } from "next/navigation";
+import DashboardSetup from "@/components/dashboard-setup/dashboard-setup";
+import { getUserSubscriptionStatus } from "@/lib/supabase/queries";
 
-const DashboardPage = () => {
-  return (
-    <div>DashboardPage</div>
-  )
-}
+const DashboardPage = async () => {
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-export default DashboardPage
+  if (!user) return;
+
+  const workspace = await db.query.workspaces.findFirst({
+    where: (workspace, { eq }) => eq(workspace.workspaceOwner, user.id),
+  });
+
+  const { data: subscription, error: subscriptionError } =
+    await getUserSubscriptionStatus(user.id);
+
+  if (subscriptionError) return;
+
+  if (!workspace) {
+    return (
+      <div className="bg-background h-screen w-screen flex justify-center items-center">
+        <DashboardSetup
+          user={user}
+          subscription={subscription}
+        ></DashboardSetup>
+      </div>
+    );
+  }
+  redirect(`/dashboard/${workspace.id}`);
+};
+
+export default DashboardPage;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -21,3 +21,11 @@ export const SignUpFormSchema = z
     message: "Passwords does not match",
     path: ["confirmPassword"],
   });
+
+export const CreateWorkspaceFormSchema = z.object({
+  workspaceName: z
+    .string()
+    .describe("Workspace Name")
+    .min(1, "Workspace name must be min of 1 character"),
+  logo: z.any(),
+});
